4ff0333857a217853801d891d58e84729b5fda66
misnosugianto48 <misnosugianto48@gmail.com>
2023-09-01 21:47:12 +0700

    JR-01092023: Fix Load Env and Implemented PlaylistsService

--- src/PlaylistsService.js
+++ src/PlaylistsService.js
@@ -5,27 +5,38 @@ class PlaylistsService {
     this._pool = new Pool();
   }
 
-  async getPlaylistsWithSongs(playlistId) {
-    const playlist = {
+  async getPlaylistsWithSongs(id) {
+    const playlistQuery = {
       text: 'SELECT playlists.id, playlists.name FROM playlists WHERE playlists.id = $1',
-      values: [playlistId],
+      values: [id],
     };
 
-    const song = {
+    const songsQuery = {
       text: 'SELECT songs.id, songs.title, songs.performer FROM songs LEFT JOIN playlistsongs ON songs.id = playlistsongs.song_id LEFT JOIN playlists ON playlistsongs.playlist_id = playlists.id WHERE playlistsongs.playlist_id = $1',
-      values: [playlistId],
+      values: [id],
     };
 
-    // ambil result kedua query
-    const playlists = await this._pool.query(playlist);
-    const songs = await this._pool.query(song);
+    // Eksekusi kedua query
+    const playlistResult = await this._pool.query(playlistQuery);
+    const songsResult = await this._pool.query(songsQuery);
+
+    // Dapatkan hasil query sebagai objek
+    const playlistData = playlistResult.rows[0];
+    const songsData = songsResult.rows;
+
+    // Buat objek sesuai format yang diinginkan
+    const formattedPlaylist = {
+      id: playlistData.id,
+      name: playlistData.name,
+      songs: songsData.map((song) => ({
+        id: song.id,
+        title: song.title,
+        performer: song.performer,
+      })),
+    };
 
     return {
-      playlist: {
-        id: playlists.rows[0].id,
-        name: playlists.rows[0].name,
-        songs: songs.rows,
-      },
+      playlist: formattedPlaylist,
     };
   }
 }
--- src/utils/config.js
+++ src/utils/config.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const config = {
   rabbitMq: {
     server: process.env.RABBITMQ_SERVER,
